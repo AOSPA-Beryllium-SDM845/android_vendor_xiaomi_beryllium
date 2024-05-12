b544eebd84749268abbe0b973d45d8163a16e224
ULTIMATE-RITESH <ultimateritesh987@gmail.com>
2024-05-13 01:12:23 +0530

    beryllium: Configuration for spatial audio
    - updated spatial audio blobs from cheetah-14-AP1A.240405.002
    
    Signed-off-by: ULTIMATE-RITESH <ultimateritesh987@gmail.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- beryllium-vendor.mk
+++ beryllium-vendor.mk
@@ -987,7 +987,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/beryllium/proprietary/vendor/lib64/soundfx/libswdap.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libswdap.so \
     vendor/xiaomi/beryllium/proprietary/vendor/lib64/soundfx/libswgamedap.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libswgamedap.so \
     vendor/xiaomi/beryllium/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0-impl.so \
-    vendor/xiaomi/beryllium/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0.so
+    vendor/xiaomi/beryllium/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0.so \
+    vendor/xiaomi/beryllium/proprietary/vendor/lib/soundfx/libspatialaudio.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libspatialaudio.so \
 
 PRODUCT_PACKAGES += \
     DaxUI \
